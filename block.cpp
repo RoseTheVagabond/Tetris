624ed1e7de869a4241f38483af2c7d2ae7178c54
RoseTheVagabond <rosethevagabond@gmail.com>
2024-08-17 12:35:42 +0200

    Column and row offset implemented

--- block.cpp
+++ block.cpp
@@ -4,6 +4,8 @@ Block::Block() {
     cellSize = 30;
     rotation = 0;
     colors = getCellColours();
+    rowOffset = 0;
+    columnOffset = 0;
 }
 
 auto Block::draw() -> void {
@@ -11,4 +13,8 @@ auto Block::draw() -> void {
     for(Position item : tiles) {
         DrawRectangle(item.column * cellSize + 1, item.row * cellSize + 1, cellSize - 2, cellSize - 2, colors[3]);
     }
+}
+
+void Block::move(int rows, int columns) {
+
 }
--- block.h
+++ block.h
@@ -10,8 +10,11 @@ public:
     int id;
     std::map<int, std::vector<Position>> cells;
     void draw();
+    void move(int rows, int columns);
 private:
     int cellSize;
     int rotation;
     std::vector<Color> colors;
+    int rowOffset;
+    int columnOffset;
 };
